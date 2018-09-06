aeda25da9afd70781602d45978b81ef034a11378
HuntsmanRoym <grins008@morris.umn.edu>
2018-09-05 20:33:34 -0500

    Code worked whole time! I just programmed too much today, and got the arguemnt numbers for my variables mixed up.

--- wrap_contents.sh
+++ wrap_contents.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-header_file=$1_header.html
-footer_file=$1_footer.html
-text_file=$2
+header_file=$2_header.html
+footer_file=$2_footer.html
+text_file=$1
 resulting_file=$3
 
 # Wrap .txt file in-between two html files to form target file
-cat $header_file $text_file $footer_file > $resulting_file  
+cat $header_file $text_file $footer_file > $resulting_file
